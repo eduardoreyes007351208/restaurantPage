ad84a0a8267045b510153e795715bf70dfd20908
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-04-29 19:25:19 -0700

    feat: Style the About Page

--- src/modules/aboutPage.js
+++ src/modules/aboutPage.js
@@ -9,13 +9,13 @@ import mapImage from '../images/map.jpg'
 export const aboutPage = () => {
 
     content.innerHTML = `
-        <div id="contentText">
-            <div>
-                <h2><em>About Us</em></h2>
-                <h3>Call us at: (123)456-7890</h3>
-                <h3>Located at: 1234 Lipsum Ave CA, 92346</h3>
-                <img src=${mapImage} alt="Map Image">
-            </div>
+        <div id="aboutPageDiv">
+            
+            <h2><em>About Us</em></h2>
+            <h3>Call us at: (123)456-7890</h3>
+            <h3>Located at: 1234 Lipsum Ave CA, 92346</h3>
+            <img class="image" src=${mapImage} alt="Map Image">
+            
         </div>
     `
 
