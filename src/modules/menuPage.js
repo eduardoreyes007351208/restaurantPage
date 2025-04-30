ff745b3a19615e6d36eec1094fb10e15ab8dd149
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-04-29 19:16:16 -0700

    feat: Style Menu Page

--- src/modules/menuPage.js
+++ src/modules/menuPage.js
@@ -19,15 +19,19 @@ export const menuPage = () => {
     addRecipe('Blueberry Bowl', 5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget elit ac ligula porta semper tincidunt nec lorem. Curabitur at.', menuList)
     addRecipe('Raspberry Bowl', 5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget elit ac ligula porta semper tincidunt nec lorem. Curabitur at.', menuList)
     addRecipe('HoneyBowl', 7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget elit ac ligula porta semper tincidunt nec lorem. Curabitur at.', menuList)
+    addRecipe('Acai Bowl Combo', 18, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget elit ac ligula porta semper tincidunt nec lorem. Curabitur at.', menuList)
+    addRecipe('Blueberry Bowl Combo', 10, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget elit ac ligula porta semper tincidunt nec lorem. Curabitur at.', menuList)
+    addRecipe('Raspberry Bowl Combo', 10, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget elit ac ligula porta semper tincidunt nec lorem. Curabitur at.', menuList)
+    addRecipe('HoneyBowl Combo', 14, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget elit ac ligula porta semper tincidunt nec lorem. Curabitur at.', menuList)
 
-    content.innerHTML = '<h1>Menu</h1> <ul>' + menuList.map((menuItem) => {
-        return `<li>
+    content.innerHTML = '<h1 id="menuPageTitle">Menu</h1> <div> <ul id="menuContainer">' + menuList.map((menuItem) => {
+        return `<li class="listItems">
             <div class="menuCard">
-                <h2>${menuItem.name}</h2>
-                <span>${menuItem.price}</span>
+                <h2 class="menuCardTitle">${menuItem.name}</h2>
+                <span class="menuCardPrice">Price: $${menuItem.price}</span>
                 <p>${menuItem.desc}</p>
             </div>
         </li>`
-    }).join('') + '</ul>'
+    }).join('') + '</ul>' + '</div>'
 
 }
